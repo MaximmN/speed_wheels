4a8adf4d30d85d8f1a0061a96a5ed8a1dfa6f3b7
MaximmN <maxim.nechai@gmail.com>
2014-10-31 10:45:34 +0300

    Specification price (migrate/products)

--- depot/db/migrate/20141031072211_create_products.rb
+++ depot/db/migrate/20141031072211_create_products.rb
@@ -5,7 +5,7 @@ class CreateProducts < ActiveRecord::Migration
       t.string :description
       t.string :text
       t.string :image_url
-      t.decimal :price
+      t.decimal :price, precision:8, scale:2
 
       t.timestamps
     end
