9614862691185de7b70da1bb85d14c968a092675
MaximmN <maxim.nechai@gmail.com>
2014-11-02 11:50:06 +0300

    Realization of detailing order.

--- depot/test/controllers/orders_controller_test.rb
+++ depot/test/controllers/orders_controller_test.rb
@@ -18,6 +18,11 @@ class OrdersControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
+    item = LineItem.new
+    item.build_cart
+    item.product = products(:ruby)
+    item.save!
+    session[:cart_id] = item.cart.id
     get :new
     assert_response :success
   end
@@ -26,8 +31,7 @@ class OrdersControllerTest < ActionController::TestCase
     assert_difference('Order.count') do
       post :create, order: { address: @order.address, email: @order.email, name: @order.name, pay_type: @order.pay_type }
     end
-
-    assert_redirected_to order_path(assigns(:order))
+    assert_redirected_to store_path
   end
 
   test "should show order" do
